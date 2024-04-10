b4f19fae3f21460db34ff3ccb425602b3073f5dc
viktech <vikrez2021@gmail.com>
2024-04-10 06:38:49 -0700

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -41,7 +41,7 @@ googleLogin.addEventListener("click", function () {
       const credential = GoogleAuthProvider.credentialFromResult(result);
       const user = result.user;
       console.log(user);
-      window.location.href = "../dashboard.html";
+      window.location.href = "..test/dashboard.html";
     })
     .catch((error) => {
       const errorCode = error.code;
